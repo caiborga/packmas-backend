6a4fefe94e5635e7dd4cdd7941f968bd27aa6350
Stephan Kleinhans <4.da.crap@gmail.com>
2025-03-05 08:39:22 +0100

    changed cors

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use(express.json());
 // });
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://packmas-c545d34ac462.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://packmas-c545d34ac462.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
     next();
